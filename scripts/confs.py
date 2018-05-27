3a27397af8c4d92fae043aa1b27685cb7c49c5dd
FighterNan <nanzhoumails@gmail.com>
2018-05-27 20:47:51 +0800

    add smart update scripts

---
+++ scripts/confs.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+    Description : configurations
+    Author      : Nan Zhou
+    Date        : May 18, 2018
+"""
+RULE_PATH = "test/"
+SMART_UPDATE = "../cmake-build-debug/SmartUpdate"
+TRACE_GENERATOR = "../cmake-build-debug/trace_generator"
+RANGE_TO_PREFIX = "range2prefix.py"
+REMOVE_ID = "remove_id.py"
+ADD_ID = "add_id.py"
+GROUP_NAME = {
+    "small_small": "ss",
+    "small_large": "sl",
+    "large_small": "ls",
+    "large_large": "ll",
+}
+MAX_RULES_PER_SET = 3000
---
+++ scripts/utils.py
@@ -0,0 +1,83 @@
+#! /usr/bin/python
+
+# -*- coding: utf-8 -*-
+"""
+    Description : useful functions
+    Author      : Nan Zhou
+    Date        : May 18, 2018
+"""
+import subprocess
+import confs
+
+# format metric_name:number
+def get_info(str_prefix, output_str_list):
+    number = None
+    for output_str in output_str_list:
+        if output_str.startswith(str_prefix):
+            temp_str = output_str.replace(str_prefix,"")
+            number = temp_str.strip(" ")
+    if number is not None:
+        return float(number)
+    else:
+        return 0
+
+def load_rule_set(rule_set_path):
+    with open(rule_set_path, 'r') as fin:
+        rules_list = fin.readlines()
+    return rules_list
+
+def range2prefix(rule_set_path, output_path):
+    subprocess.getoutput("python " + confs.RANGE_TO_PREFIX + " "+ output_path + " "+rule_set_path+"_p")
+    return (rule_set_path+"_p")
+
+def remove_id(rule_set_path, output_path):
+    rule_set_name = rule_set_path.split("/")
+    rule_set_name = rule_set_name[len(rule_set_name)-1]
+    subprocess.getoutput("python " + confs.REMOVE_ID + " " + rule_set_path +" "+output_path+rule_set_name)
+    return (output_path+rule_set_name)
+
+def add_id(rule_set_path):
+    subprocess.getoutput("python " + confs.ADD_ID + " " + rule_set_path)
+    return rule_set_path
+
+def combine_rule_sets(rule_set_path_list):
+    rule_set_combined = rule_set_path_list[0]+"_updated"
+    with open(rule_set_combined, 'w') as fout:
+        for i in range(0, len(rule_set_path_list)):
+            rules = []
+            with open(rule_set_path_list[i], 'r') as fin:
+                for i in fin.readlines():
+                    rules.append(i)
+            for rule in rules:
+                fout.write('%s' % rule)
+    for rule_set_path in rule_set_path_list:
+        subprocess.getoutput("rm "+rule_set_path)
+    return rule_set_combined
+
+def generate_traces(rule_set_path):
+    subprocess.getoutput("./" + confs.TRACE_GENERATOR + " 1 0 10 " + rule_set_path)
+    return rule_set_path+"_trace"
+
+def generate_combined_trace(rule_set_path, update_rule_set_path, out_path):
+    rule_set_path_temp = remove_id(rule_set_path, out_path)
+    update_rule_set_path_temp = remove_id(update_rule_set_path, out_path)
+    updated_rule_set = combine_rule_sets([rule_set_path_temp, update_rule_set_path_temp])
+    traces_path = generate_traces(updated_rule_set)
+    add_id(updated_rule_set)
+    return updated_rule_set, traces_path
+
+def hs_build(rule_set_path, traces):
+    return_strs = subprocess.getoutput("./" + confs.SMART_UPDATE + " -a -0" + " -r " + rule_set_path + \
+                                       " -t " + traces)
+    build_time = get_info("Time for building(us):", return_strs.split('\n'))
+    search_time = get_info("Time for searching(us):", return_strs.split('\n'))
+    return build_time, search_time
+
+def hs_build_estimator(rule_set_path):
+    return_strs = subprocess.getoutput("./" + confs.SMART_UPDATE + " -a -0" + " -e 1" + " -r " + rule_set_path + " -s 1")
+    build_time = get_info("Estimated time:", return_strs.split('\n'))
+    return build_time
+
+if __name__ == "__main__":
+    str = "Time for updating(us): 1388"
+    print(get_info("Time for updating(us):", [str]))
