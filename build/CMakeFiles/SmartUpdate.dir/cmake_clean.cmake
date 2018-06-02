75d9bf5e9548b761fb2b23e916cbb529967fc9de
FighterNan <nanzhoumails@gmail.com>
2018-06-02 22:25:03 +0800

    add build folder

---
+++ build/CMakeFiles/SmartUpdate.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartUpdate.dir/code/hs.o"
+  "CMakeFiles/SmartUpdate.dir/code/mem_sim.o"
+  "CMakeFiles/SmartUpdate.dir/code/pc_eval.o"
+  "CMakeFiles/SmartUpdate.dir/code/tss.o"
+  "CMakeFiles/SmartUpdate.dir/code/utils.o"
+  "SmartUpdate.pdb"
+  "SmartUpdate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SmartUpdate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
